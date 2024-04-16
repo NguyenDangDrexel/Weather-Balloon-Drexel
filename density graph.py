cb0c8468290a8f5161e6fa12b8e4032c65567216
NguyenDangDrexel <ppd34@drexel.edu>
2024-04-16 10:55:32 -0400

    Update density graph.py

--- density graph.py
+++ density graph.py
@@ -1,15 +1,32 @@
 import matplotlib.pyplot as plt 
 import numpy as np 
 from Model import *
+#This program will plot the graph: H1 <= h <=  H2
 
-data = np.concatenate((np.linspace(0,11000,100), np.linspace(11000,25000,100), np.linspace(25000,35000,100)))
+H1 = 0 
+H2 = 35000
+# data_points that you want to make
+data_points = 10000
 
-density = np.concatenate((density1(np.linspace(0,11000,100)), density2(np.linspace(11000,25000,100)), density3(np.linspace(25000,35000,100))))
 
-plt.plot (data,density)
+density_data = []
+
+data = np.linspace (H1,H2,data_points)
+
+for i in range (data_points):
+    density_data.append(Density(data[i]))
+
+
+plt.plot (data,density_data)
 
 plt.title ('Density vs altitude')
 plt.xlabel ('Altitude (m)')
 plt.ylabel ('Density kg/m^3')
 plt.legend ()
 plt.show ()  
+
+
+
+
+
+
